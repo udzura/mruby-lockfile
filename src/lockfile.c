171b765a38c63b92ab958df09977330fbfc13af9
Uchio KONDO <udzura@pepabo.com>
2017-06-22 23:08:19 +0900

    Implement Pidfiles

--- src/lockfile.c
+++ src/lockfile.c
@@ -8,6 +8,7 @@
 
 #include <errno.h>
 #include <fcntl.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -25,6 +26,7 @@
 typedef struct {
   char *path;
   int fd;
+  pid_t pid; /* unused for Lockfile */
 } mrb_lockfile_data;
 
 void mrb_lockfile_free(mrb_state *mrb, void *p)
@@ -58,6 +60,7 @@ static mrb_value mrb_lockfile_init(mrb_state *mrb, mrb_value self)
   if (data->fd < 0) {
     mrb_sys_fail(mrb, "initial open");
   }
+  data->pid = -1;
 
   DATA_PTR(self) = data;
 
@@ -176,9 +179,77 @@ static mrb_value mrb_lockfile_trylock(mrb_state *mrb, mrb_value self)
   return mrb_true_value();
 }
 
+/* Pidfile: Lockfile's subclass to control pidfile */
+static mrb_value mrb_pidfile_write(mrb_state *mrb, mrb_value self)
+{
+  pid_t cur = getpid();
+  size_t len = 2;
+  char *buf;
+  int wk = (int)cur, wk2, detect_fail = 0;
+  while ((wk = wk / 10) != 0)
+    len++;
+
+  buf = mrb_malloc(mrb, len);
+  snprintf(buf, len, "%d", cur);
+
+  mrb_lockfile_data *data = DATA_PTR(self);
+
+  if (ftruncate(data->fd, 0) < 0)
+    mrb_sys_fail(mrb, "mrb_pidfile_write: ftruncate");
+
+  wk = 0;
+  while (wk < len) {
+    wk2 = write(data->fd, buf, len);
+    if (wk2 < 0)
+      detect_fail = 1;
+    wk += wk2;
+  }
+  mrb_free(mrb, buf);
+  if (detect_fail)
+    mrb_sys_fail(mrb, "mrb_pidfile_write: write");
+
+  if (fsync(data->fd) < 0)
+    mrb_sys_fail(mrb, "mrb_pidfile_write: fsync");
+
+  data->pid = cur;
+  return mrb_fixnum_value(cur);
+}
+
+static mrb_value mrb_pidfile_pid(mrb_state *mrb, mrb_value self)
+{
+  struct flock f = {.l_type = 0};
+  mrb_lockfile_data *data = DATA_PTR(self);
+  mrb__get_flock(mrb, data->fd, &f);
+  if (f.l_type == F_UNLCK) {
+    if (data->pid < 0) {
+      data->pid = getpid();
+    }
+    return mrb_fixnum_value(data->pid);
+  }
+
+  {
+#define PID_BUF_MAX 16
+    int pid, readfd;
+    char buf[PID_BUF_MAX];
+    memset(buf, 0, PID_BUF_MAX);
+
+    readfd = open(data->path, O_RDONLY);
+    if (read(readfd, buf, PID_BUF_MAX) < 0)
+      mrb_sys_fail(mrb, "mrb_pidfile_pid: read");
+    close(readfd);
+
+    pid = strtol(buf, NULL, 0);
+    if (pid > 0) {
+      return mrb_fixnum_value(pid);
+    } else {
+      return mrb_nil_value();
+    }
+  }
+}
+
 void mrb_mruby_lockfile_gem_init(mrb_state *mrb)
 {
-  struct RClass *lockfile;
+  struct RClass *lockfile, *pidfile;
   lockfile = mrb_define_class(mrb, "Lockfile", mrb->object_class);
   MRB_SET_INSTANCE_TT(lockfile, MRB_TT_DATA);
   mrb_define_method(mrb, lockfile, "initialize", mrb_lockfile_init, MRB_ARGS_REQ(1));
@@ -191,6 +262,10 @@ void mrb_mruby_lockfile_gem_init(mrb_state *mrb)
   mrb_define_method(mrb, lockfile, "locking_pid", mrb_lockfile_locking_pid, MRB_ARGS_NONE());
 
   mrb_define_class_method(mrb, lockfile, "exist?", mrb_lockfile_exists, MRB_ARGS_REQ(1));
+
+  pidfile = mrb_define_class(mrb, "Pidfile", lockfile);
+  mrb_define_method(mrb, pidfile, "write", mrb_pidfile_write, MRB_ARGS_NONE());
+  mrb_define_method(mrb, pidfile, "pid", mrb_pidfile_pid, MRB_ARGS_NONE());
 
   DONE;
 }
