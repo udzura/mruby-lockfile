5c18650d5efb83b0907681d91c20ea001bc0152b
Uchio KONDO <udzura@pepabo.com>
2017-06-22 16:18:47 +0900

    More test

--- bintest/lockfile.rb
+++ bintest/lockfile.rb
@@ -26,3 +26,17 @@ assert('lockwait for another lock released') do
   system "killall mruby 2>/dev/null || true"
   system "rm -f './tmp/test102.lock'"
 end
+
+assert('trylock output') do
+  system "mkdir -p tmp"
+  output, err, status = Open3.capture3(BIN_PATH, "-e", "l = Lockfile.new './tmp/test103.lock'; puts l.trylock.to_s")
+  assert_equal "true", output.chomp
+
+  system %Q(#{BIN_PATH} -e "Lockfile.lock './tmp/test103.lock'; loop {}" &)
+  sleep 0.5
+  output, err, status = Open3.capture3(BIN_PATH, "-e", "l = Lockfile.new './tmp/test103.lock'; puts l.trylock.to_s")
+  assert_equal "false", output.chomp
+
+  system "killall mruby"
+  system "rm -f './tmp/test103.lock'"
+end
